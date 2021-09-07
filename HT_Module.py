c9f71872ea299d172d0d40ee394c2e1710fcd4d5
Palash <palashdhavle15@gmail.com>
2021-09-07 23:33:08 +0530

    Added Hand Tracking Module

---
+++ HT_Module.py
@@ -0,0 +1,97 @@
+import cv2 as cv
+import mediapipe as mp
+import time
+
+class Hand_Tracker():
+    def __init__(self,mode=False, max_hands=2, detetctionCon=0.5, trackingCon=0.5):
+        """initializes all the neccessary things"""
+        self.mode=mode
+        self.max_hands=max_hands
+        self.detetctionCon=detetctionCon
+        self.trackingCon=trackingCon
+
+        self.mpHands = mp.solutions.hands
+        self.hands = self.mpHands.Hands(self.mode,self.max_hands,self.detetctionCon,self.trackingCon)
+        self.mpDraw = mp.solutions.drawing_utils
+
+    
+    def get_hands(self,RGB_img,img,draw_hands=True):
+        """detects, traces and draws hands"""
+        self.results = self.hands.process(RGB_img)
+        h, w, c = img.shape
+
+        if self.results.multi_hand_landmarks:
+            for handLms in self.results.multi_hand_landmarks:
+                if draw_hands:
+                    # draws the hands on the image with connecting lines
+                    self.mpDraw.draw_landmarks(img,handLms,self.mpHands.HAND_CONNECTIONS)
+        
+        return img
+
+    def find_positions(self, img, multipleHands=True, handNo=0, draw=True):
+        """gets the indivisual points present of the hand"""
+        # creating a landmark list to store the landmarks
+        landmark_list = []
+        if self.results.multi_hand_landmarks:
+            if multipleHands:
+                # detects points on multiple hands
+                for handLms in self.results.multi_hand_landmarks:
+                    self._get_landmarks(handLms,img,landmark_list,draw)
+            else:
+                # detects points on single hand at a time
+                handLms = self.results.multi_hand_landmarks[handNo]
+                self._get_landmarks(handLms,img,landmark_list,draw)
+        
+        return landmark_list
+    
+    
+    def _get_landmarks(self,hand_landmarks,img,landmark_list,draw):
+        h, w, c = img.shape
+        for id,landmarks in enumerate(hand_landmarks.landmark):
+            
+            cx, cy = int(landmarks.x*w), int(landmarks.y*h)
+
+            # appending values to landmark_list
+            landmark_list.append([id,cx,cy])
+
+            if draw:
+                if id == 4 or id == 8:
+                    # draws on individual points
+                    cv.circle(img,(cx,cy),15,(255,255,0),-1)
+
+
+def main():
+    capture = cv.VideoCapture(0)
+
+    tracker = Hand_Tracker()
+
+    previous_time = 0
+    current_time = 0
+
+    while True:
+        isTrue, img = capture.read()
+        RGB_img = cv.cvtColor(img,cv.COLOR_BGR2RGB)
+
+        # FPS logic
+        current_time = time.time()
+        fps = 1/(current_time-previous_time)
+        previous_time = current_time
+
+        img = tracker.get_hands(RGB_img,img,True)
+        landmarks = tracker.find_positions(img,draw=True)
+        if len(landmarks)!=0:
+            print(landmarks[4])
+    
+        # fliping the image so it doesn't get displayed inverted
+        img = cv.flip(img,1)
+
+        # displaying FPS
+        cv.putText(img,f"FPS:{str(int(fps))}",(20,50),cv.FONT_HERSHEY_SIMPLEX,2,(156,222,27),2)
+
+   
+        cv.imshow("win",img)
+        if cv.waitKey(1) == ord('q'):
+            break
+
+if __name__ == "__main__":
+    main()
