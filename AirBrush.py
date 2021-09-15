ed5ea826eec6b3d94d4affa47dbbffe85150037f
Palash <palashdhavle15@gmail.com>
2021-09-16 00:56:35 +0530

    places UI elements on the screen in real time

--- AirBrush.py
+++ AirBrush.py
@@ -1,17 +1,40 @@
 import cv2 as cv
+import os
+import UI_components as UIC
 
 capture = cv.VideoCapture(0)
-hands = cv.imread(r"Images\virtuals paint.png")
-hands = cv.resize(hands,(640,71))
-h,w,c = hands.shape
-# hands = cv.resize(hands,(640,480))
+UI_components = UIC.load_UI_components("Images")
 
+# video Loop
 while True:
     _,img = capture.read() # reading image from wecam stream
     img = cv.flip(img,1) # flipping the image
-    roi = img[0:h,0:w]
-    added_roi = cv.addWeighted(roi,0.1,hands,0.9,0)
-    img[0:h,0:w] = added_roi
+
+    #looping through UI components dict
+    for keys,values in UI_components.items():
+
+        #skipping if we have the mentioned componenets
+        if keys in ["brushbuttonon","eraserbuttonon"]:
+            continue
+
+        resized = values[2]
+        bg_mask = values[4]
+        component = values[0]
+
+        if resized:
+            component = cv.resize(component,resized)
+
+        x_pos,y_pos = values[1][0],values[1][1]
+        height,width,channels = component.shape
+        masked_componenet = values[3]
+            
+        roi = img[y_pos:y_pos+height , x_pos:x_pos+width]#getting ROI
+        masked_roi = cv.bitwise_and(roi,roi,mask=bg_mask)# masking ROI(removing UI element part and keeping rest)
+
+        # adding both the images and placeing it on the exact same place where we took ROI from
+        img[y_pos:y_pos+height , x_pos:x_pos+width] = cv.add(masked_componenet,masked_roi)
+        
+
     cv.imshow("img",img)
 
     if cv.waitKey(1) == ord('q'):
